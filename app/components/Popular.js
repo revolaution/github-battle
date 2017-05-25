0c2f8d210c907714748f5b81ad15aee650f72d5e
Ryan Lau <rylau@thoughtworks.com>
2017-05-25 16:17:11 -0500

    Popular now has a list of langauge and will highlight when clicked on

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,40 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(language) {
+    this.setState(() => {
+      return {
+        selectedLanguage: language
+      }
+    });
+  }
+
   render() {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
-      <div>Popular!</div>
+      <ul className='languages'>
+        {languages.map((language) => {
+          return (
+            <li
+              style={language == this.state.selectedLanguage ? {color: '#d0021b'} : null}
+              onClick={this.updateLanguage.bind(null, language)}
+              key={language}
+            >
+              {language}
+            </li>
+          )
+        }, this)}
+      </ul>
     )
   }
 }

ca2991dc389211a1ffb8101f16367d893fd59f13
Ryan Lau <rylau@thoughtworks.com>
2017-05-26 15:26:21 -0500

    Include Switch for routes

--- app/components/App.js
+++ app/components/App.js
@@ -3,8 +3,11 @@ let Popular = require('./Popular');
 let ReactRouter = require('react-router-dom');
 let Router = ReactRouter.BrowserRouter;
 let Route = ReactRouter.Route;
+let Switch = ReactRouter.Switch;
 let Nav = require('./Nav');
 let Home = require('./Home');
+let Battle = require('./Battle');
+
 
 class App extends React.Component {
   render() {
@@ -12,8 +15,14 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Nav />
-          <Route exact path='/' component={Home} />
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={() => {
+                return <p>Not Found</p>
+              }} />
+          </Switch>
         </div>
       </Router>
     )

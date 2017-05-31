0f240875e93e306506a4b562893ba2a6d32ab0dc
Ryan Lau <rylau@thoughtworks.com>
2017-05-31 12:37:53 -0500

    Add api functionality to get user profile data

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,67 @@
 let axios = require('axios');
 
+let client_id = "YOUR_CLIENT_ID";
+let client_secret = "YOUR_SECRET_KEY";
+let params = "?client_id=" + client_id + "&client_secret" + client_secret;
+
+const getProfile = (username) => {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then((user) => {
+      return user.data;
+    })
+}
+
+const getRepos = (username) => {
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+const getStarCount = (repos) => {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+const calculateScore = (profile, repos) => {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+const handleError = (error) => {
+  console.warn(error);
+  return  null;
+}
+
+const getUserData = (player) => {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then((data) => {
+    let profile = data[0];
+    let repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+const sortPlayers = (players) => {
+  return players.sort((a, b) => {
+    return b.score - a.score;
+  })
+}
+
 module.exports = {
+  battle: (players) => {
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
+  },
   fetchPopularRepos: (language) => {
-    let encodedURI =                window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '%sort=stars&order=desc&type=Repositories');
+    let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '%sort=stars&order=desc&type=Repositories');
 
     return axios.get(encodedURI)
       .then((response) => {

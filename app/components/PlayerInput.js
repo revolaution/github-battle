1686b26b3226628bd7812942257103d7da3a41ba
Ryan Lau <rylau@thoughtworks.com>
2017-05-31 11:12:48 -0500

    Add player input form and pass onSubmit down to it

---
+++ app/components/PlayerInput.js
@@ -0,0 +1,65 @@
+let React = require('react');
+let PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let value = event.target.value;
+
+    this.setState(() => {
+      return {
+        username: value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.props.id, this.state.username)
+  }
+
+  render() {
+    return (
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+
+        <input
+          id='username'
+          placeholder='Github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+
+      <button
+        className='button'
+        type='submit'
+        disabled={!this.state.username}>
+          Submit
+      </button>
+      </form>
+    )
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+
+module.exports = PlayerInput;
